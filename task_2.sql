72345fb031dab75ba3ac2d1bf0b5491742037f78
Souhail Ainous <souhailainouss55@gmail.com>
2025-09-07 19:47:14 +0100

    updating the task_2.sql file to meet the requirements of the task

--- task_2.sql
+++ task_2.sql
@@ -2,25 +2,25 @@
 USE alx_book_store;
 
 -- AUTHORS TABLE
-CREATE TABLE IF NOT EXISTS authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT,
     author_name VARCHAR(215),
     PRIMARY KEY (author_id)
 ) ENGINE=INNODB;
 
 -- BOOKS TABLE
-CREATE TABLE IF NOT EXISTS books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT,
     title VARCHAR(130),
     author_id INT,
     price DOUBLE,
     publication_date DATE,
     PRIMARY KEY (book_id),
-    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 ) ENGINE=INNODB;
 
 -- CUSTOMERS TABLE
-CREATE TABLE IF NOT EXISTS customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT,
     customer_name VARCHAR(215),
     email VARCHAR(215),
@@ -29,21 +29,21 @@ CREATE TABLE IF NOT EXISTS customers (
 ) ENGINE=INNODB;
 
 -- ORDERS TABLE
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT,
     customer_id INT,
     order_date DATE,
     PRIMARY KEY (order_id),
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 ) ENGINE=INNODB;
 
 -- ORDER_DETAILS TABLE
-CREATE TABLE IF NOT EXISTS order_details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
     PRIMARY KEY (orderdetailid),
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 ) ENGINE=INNODB;
